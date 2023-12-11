dba2b090ad67fae38e261b11a75cca9689c5a9ef
crepefeu <esteban.rodriguez@live.fr>
2023-12-11 08:40:46 +0100

    Added sign-in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './app-sections/website/pages/home/home.component';
 import { EducationComponent } from './app-sections/website/pages/education/education.component';
 import { AdminDashboardComponent } from './app-sections/dashboard/admin-dashboard/admin-dashboard.component';
-import { OverviewComponent } from './app-sections/dashboard/overview/overview.component';
+import { OverviewComponent } from './app-sections/dashboard/pages/overview/overview.component';
 import { IndexComponent } from './app-sections/website/index/index.component';
+import { SignInComponent } from './app-sections/website/pages/sign-in/sign-in.component';
 
 const routes: Routes = [
   {
@@ -13,10 +14,11 @@ const routes: Routes = [
     children: [
       { path: '', component: HomeComponent},
       { path: 'but-mmi', component: EducationComponent},
+      { path: 'admin/sign-in', component: SignInComponent}
     ]
   },
   {
-    path: 'dashboard',
+    path: 'admin/dashboard',
     component: AdminDashboardComponent,
     // canActivate: [AuthGuard],
     children: [
