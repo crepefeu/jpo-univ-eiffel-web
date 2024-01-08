81782e5f4631f15472562461f1f5819379e23736
crepefeu <esteban.rodriguez@live.fr>
2024-01-08 17:19:17 +0100

    - Removed Infos pratiques from header - Ajusted padding on homepage - Observed url to detect when the page is the virtual tour to remove the header and the footer

--- src/app/app-sections/website/index/index.component.ts
+++ src/app/app-sections/website/index/index.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
 
+  displayHeaderAndFooter = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    // Observe url changes and hide header on virtual tour page
+    this.router.events.subscribe((val) => {
+      if (this.router.url.includes('virtual-tour')) {
+        this.displayHeaderAndFooter = false;
+      } else {
+        this.displayHeaderAndFooter = true;
+      }
+    });
+    }
 }
