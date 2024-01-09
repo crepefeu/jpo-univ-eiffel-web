f0bf36782c6ffaeb0e644087a1b0e089187d7753
crepefeu <esteban.rodriguez@live.fr>
2024-01-09 23:32:55 +0100

    Added imports for gsap and cleaned ngOnInit hook

--- src/app/app-sections/website/pages/faq/faq.component.ts
+++ src/app/app-sections/website/pages/faq/faq.component.ts
@@ -1,4 +1,6 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-faq',
@@ -6,11 +8,6 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
-
-  // @HostListener('body:pointermove', ['$event'])
-  // onPointerMove(event: PointerEvent) {
-  //   this.syncPointer({ x: event.clientX, y: event.clientY });
-  // }
 
   ngOnInit(): void {
     if (!CSS.supports('animation-timeline: scroll()')) {
@@ -39,55 +36,6 @@ export class FaqComponent implements OnInit {
       })
 
     }
-    //     gsap.registerPlugin(ScrollTrigger)
-    //     gsap.set('section', { '--base': 0 })
-    //     gsap.to('section', {
-    //       '--base': 320,
-    //       ease: 'none',
-    //       scrollTrigger: {
-    //         horizontal: true,
-    //         scrub: true,
-    //         scroller: 'ul',
-    //       },
-    //     })
-    //     const ITEMS = document.querySelectorAll('li')
-    //     ITEMS.forEach((ITEM) => {
-    //       gsap
-    //         .timeline()
-    //         .set(ITEM, { '--sat': 0 })
-    //         .to(ITEM, {
-    //           '--sat': 100,
-    //           scrollTrigger: {
-    //             trigger: ITEM,
-    //             start: 'right 75%',
-    //             end: 'center center',
-    //             horizontal: true,
-    //             scrub: true,
-    //             scroller: 'ul',
-    //           },
-    //         })
-    //         .fromTo(
-    //           ITEM,
-    //           { '--sat': 100 },
-    //           {
-    //             '--sat': 0,
-    //             scrollTrigger: {
-    //               trigger: ITEM,
-    //               end: 'left 25%',
-    //               start: 'center center',
-    //               horizontal: true,
-    //               scrub: true,
-    //               scroller: 'ul',
-    //             },
-    //           }
-    //         )
-    //     })
-    // }
-
-    // syncPointer = ({ x, y }: { x: number, y: number }) => {
-    //   document.documentElement.style.setProperty('--px', x.toFixed(2))
-    //   document.documentElement.style.setProperty('--py', y.toFixed(2))
-    // }
   }
 
   goToContent(contentId: string) {
