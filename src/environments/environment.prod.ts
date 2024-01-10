00b1d0667535e748f8266c53fb1b643bfd53dd69
crepefeu <esteban.rodriguez@live.fr>
2024-01-10 15:04:36 +0100

    Increased budgets Fixed filename for production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseApiUrl: 'https://api.sae-501-jpo.fr/',
+    baseApiUrl: 'https://api.sae-501-jpo.fr/api/',
     documentsUrl: 'https://api.sae-501-jpo.fr/documents/',
 };
