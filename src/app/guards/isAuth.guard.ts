e98897065cc8e4a36780c822c19e1e9e8f7436a3
crepefeu <esteban.rodriguez@live.fr>
2025-02-23 14:26:06 +0100

    Enhance authentication guard to check for token presence and improve verification logic

--- src/app/guards/isAuth.guard.ts
+++ src/app/guards/isAuth.guard.ts
@@ -1,20 +1,25 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, UrlTree } from '@angular/router';
 import { AdminsService } from '../services/admins.service';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 export function isAuth(): CanActivateFn {
+
   return (isAuth): Observable<boolean | UrlTree> => {
     const adminService: AdminsService = inject(AdminsService);
     const router: Router = inject(Router);
-    return adminService.verifyAuth().pipe(
-      map((data: boolean) => {
-        if (data) {
-          return true;
-        } else {
-          return router.createUrlTree(["/admin", "sign-in"]);
-        }
-      })
-    );
+    if (!localStorage.getItem('token')) {
+      return of(router.createUrlTree(["/admin", "sign-in"]));
+    } else {
+      return adminService.verifyAuth().pipe(
+        map((data: any) => {
+          if (data && data.isAuthenticated == true) {
+            return true;
+          } else {
+            return router.createUrlTree(["/admin", "sign-in"]);
+          }
+        })
+      );
+    }
   };
 }
